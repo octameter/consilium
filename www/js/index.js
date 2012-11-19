818e8f0befe796ca77745611c9e3d291d39e2353
Marco Egbring <marco.egbring@epha.ch>
2012-11-19 17:15:54 +0100

    Config.xml

--- www/js/index.js
+++ www/js/index.js
@@ -17,33 +17,26 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        this.bind();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    bind: function() {
+        document.addEventListener('deviceready', this.deviceready, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+    deviceready: function() {
+        // This is an event handler function, which means the scope is the event.
+        // So, we must explicitly called `app.report()` instead of `this.report()`.
+        app.report('deviceready');
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    report: function(id) {
+        // Report the event in the console
+        console.log("Report: " + id);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        // Toggle the state from "pending" to "complete" for the reported ID.
+        // Accomplished by adding .hide to the pending element and removing
+        // .hide from the complete element.
+        document.querySelector('#' + id + ' .pending').className += ' hide';
+        var completeElem = document.querySelector('#' + id + ' .complete');
+        completeElem.className = completeElem.className.split('hide').join('');
     }
 };
