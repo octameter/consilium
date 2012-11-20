c611b211549ed0759f78a55177e2af440d531976
Marco Egbring <marco.egbring@epha.ch>
2012-11-20 15:40:34 +0100

    Icon moved

--- js/index.js
+++ js/index.js
@@ -72,21 +72,9 @@ var app = {
         }
     },
     photo: function() {
-    	// capture callback
-    	var captureSuccess = function(mediaFiles) {
-    	    var i, path, len;
-    	    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-    	        path = mediaFiles[i].fullPath;
-    	        // do something interesting with the file
-    	    }
-    	};
+   	
+   	    navigator.notification.alert("Hallo", null, 'Capture Error');
 
-    	// capture error callback
-    	var captureError = function(error) {
-    	    navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-    	};
-    	
-    	navigator.device.capture.captureImage(captureSuccess, captureError, {limit:2});
     }
 
 };
