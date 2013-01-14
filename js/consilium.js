4ea0aef04edc4adc1688a9d6bb78221ee18f464e
Marco Egbring <marco.egbring@epha.ch>
2013-01-14 11:55:17 +0100

    Correct gitignore

---
+++ js/consilium.js
@@ -0,0 +1,44 @@
+function Consilium() {
+	/**
+	 * MODULES
+	 */
+	
+	/**
+	 * VIEWS
+	 */
+	this.home = new Home();
+	
+	this.symptome = new Symptome();
+} 
+
+	/**
+	 * GLOBAL VARIABLES
+	 */
+
+	/**
+	 * Set Module & Views
+	 */	
+	Consilium.prototype.init = function(elementId){	
+		this.home.init(elementId);		
+		
+		this.symptome.init(elementId);
+	};
+	
+	Consilium.prototype.addViewSymptome = function(){
+		this.home.setViewSymptome( this.symptome);
+
+		this.symptome.setViewBack( this.home );
+	};
+
+	/**
+	 * Modul Symptome
+	 */
+	
+	/**
+	 * VIEW HOME
+	 */
+	Consilium.prototype.texto = function(){
+		
+	};
+	
+	
